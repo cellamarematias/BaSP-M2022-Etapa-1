72d5b13f01c54d2a76c46ef8c7f95dfc494f7558
Cellamare Matías <cellamarematias@gmail.com>
2022-04-18 17:09:09 -0300

    Basp-2022: ex01-variables and operators.

---
+++ Semana-05/exercises/01_variables_and_operators.js
@@ -0,0 +1,15 @@
+//a
+var firstNumber = 1;
+var secondNumber = 2;
+var addition = firstNumber + secondNumber;
+console.log(addition);
+//b
+var firstWord = 'Tengo ganas de estudiar, ';
+var secondWord = 'pero me las aguanto.';
+var finalText = firstWord + secondWord;
+console.log(finalText);
+//c
+var firstString = 'hola';
+var secondString = 'mundo';
+var stringslengths = firstString.length + secondString.length;
+console.log(stringslengths);
